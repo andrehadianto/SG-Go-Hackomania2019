19c120e685e247c2b67b5e62aa3766316de86fce
andrehadianto <andre_hadianto@mymail.sutd.edu.sg>
2019-02-23 20:50:58 +0800

    Resolved EventsActivity duplicate

--- app/src/main/java/com/example/celineyee/kongsimi/EventsActivity.java
+++ app/src/main/java/com/example/celineyee/kongsimi/EventsActivity.java
@@ -1,7 +1,5 @@
 package com.example.celineyee.kongsimi;
 
-public class EventsActivity {
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
